f4a4549a9399526c840bbd9ae215132bed8580a5
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2025-02-16 13:27:06 +0530

    Create Array-DS.c

---
+++ Array-DS.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void RevArr(int n , int arr[], int result[]){
+    for (int i=n-1;i>=0;i--){
+        result[n-1-i] = arr[i];
+    }
+}  
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int result[n];
+    RevArr(n,arr,result);
+    for (int i=0;i<n;i++){
+        printf("%d ",result[i]);    
+    }
+    
+    return 0;
+}
